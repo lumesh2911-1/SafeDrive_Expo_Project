05e332d3e2017f70b2ab602fb2f706d017ae5946
lumesh2911-1 <lumesh2911@gmail.com>
2026-06-01 21:50:41 +0530

    All files are structured

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
---
+++ src/app/summary.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Summary() {
+  return (
+    <View>
+      <Text>S</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
